7fdb5813bdf2c0342264b7122054fffb59e6dba1
Manonmani13 <manonmani.pandukuma@gmail.com>
2024-04-13 22:58:28 +0530

    custom key generator

---
+++ 1stSpringBootProkect/src/main/java/com/learning/repository/OrderDetailsRepo.java
@@ -0,0 +1,10 @@
+package com.learning.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learning.entity.OrderDetailsEntity;
+
+public interface OrderDetailsRepo extends JpaRepository<OrderDetailsEntity, String> {
+
+}
